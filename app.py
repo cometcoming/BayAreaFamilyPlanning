6af4459ae9edc2ee4be8e1af13ffa0d4701a659f
cometcoming <143593086+cometcoming@users.noreply.github.com>
2026-06-10 00:56:22 -0700

    project structure

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.title("Bay Area Household Planner")
 
+st.subheader("Necessary Information")
+
 city = st.selectbox(
     "City of Interest",
     ["San Jose", "San Francisco", "Oakland", "Santa Clara", "Sunnyvale", "Mountain View", "Palo Alto", "Cupertino", "Milpitas", "Saratoga", "Los Gatos", "Daly City", "San Mateo", "Redwood City", "South San Francisco", "Burlingame", "Menlo Park", "Palo Alto", "Los Altos", "Foster City", "Berkeley", "Fremont", "Hayward", "Concord", "Richmond", "Pleasanton", "Walnut Creek", "Livermore", "Dublin", "Alameda", "San Leandro", "Antioch", "Pittsburg", "Martinez"]
@@ -35,9 +37,31 @@ vehicles = st.slider(
     1
 )
 
+for i in range(vehicles):
+    with st.expander(f"Vehicle {i+1} Ownership Status"):
+        status = st.radio("Ownership", ["Owned", "Leased", "Rented"], key=f"vehicle_radio_{i}", label_visibility="collapsed")
+
+properties = st.slider(
+    "Number of (Bay Area) Properties",
+    0,
+    5,
+    1
+)
+
+for i in range(properties):
+    with st.expander(f"Property {i+1} Ownership Status"):
+        status = st.radio("Ownership", ["Owned", "Leased", "Rented"], key=f"property_radio_{i}", label_visibility="collapsed")
+
+st.subheader("Recommended Information")
+
+
+
+st.subheader("Optional Information")
+
+
 st.write("### Your Inputs")
 
 st.write("City:", city)
-st.write("Income:", income)
+#st.write("Income:", income)
 st.write("Children:", children)
 st.write("Vehicles:", vehicles)
